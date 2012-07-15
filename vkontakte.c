8adad7752da528173b9c184527fdf4ad7d6c2fd2
scorpp <scorpp@github>
2012-07-15 23:45:56 +0300

    sync

--- vkontakte.c
+++ vkontakte.c
@@ -1,14 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
 #include <gtk/gtk.h>
-#include "../../deadbeef.h"
-#include "../gtkui/gtkui_api.h"
+#include <deadbeef.h>
+#include <plugins/gtkui/gtkui_api.h>
+#include <curl/curl.h>
 
 #define trace(...) { fprintf(stderr, __VA_ARGS__); }
+
 #ifdef ENABLE_NLS
 #include <libintl.h>
 #define _(X) gettext(X)
 #else
 #define _(X) (X)
 #endif
+
+#define VK_AUTH_URL "http://oauth.vkontakte.ru/authorize?client_id=3035566\
+					&scope=audio,friends\
+					&redirect_uri=http://scorpspot.blogspot.com\
+					&response_type=token"
+#define VK_API_URL "https://api.vk.com/method/"
 
 static DB_functions_t *deadbeef;
 static ddb_gtkui_t *gtkui_plugin;
@@ -17,18 +27,51 @@ static intptr_t http_tid;	// thread for communication
 
 static GtkWidget *add_tracks_dlg;
 
+static char *vk_access_token = "3e0d03b76e4e32e63e11cff96e3e62d1d133e4c3e4c8075989745e493ce36b8";
+
 typedef struct {
 	const gchar *query;
 } SEARCH_QUERY;
 
+size_t 
+http_write_data( char *ptr, size_t size, size_t nmemb, void *userdata) {
+	trace(ptr);
+	return size * nmemb;
+}
+
 void 
 http_thread_func(void *ctx) {
+	trace("Http thread started\n");
+	CURL *curl = curl_easy_init();
 	
+	char *curl_err_buf = malloc(CURL_ERROR_SIZE);
+	char *method_url = malloc(strlen(VK_API_URL) + strlen(vk_access_token) + 24);
+	sprintf(method_url, "%s%s%s", VK_API_URL, "audio.get?access_token=", vk_access_token);
+	curl_easy_setopt(curl, CURLOPT_URL, method_url);
+	curl_easy_setopt(curl, CURLOPT_USERAGENT, "deadbeef");
+	curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+	// enable up to 10 redirects
+    curl_easy_setopt (curl, CURLOPT_FOLLOWLOCATION, 1);
+	curl_easy_setopt (curl, CURLOPT_MAXREDIRS, 10);
+	// setup handlers
+	curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, http_write_data);
+	curl_easy_setopt (curl, CURLOPT_ERRORBUFFER, curl_err_buf);
+	
+	int status = curl_easy_perform(curl);
+	trace("curl_easy_perform(..) returned %d\n", status);
+	if (status != 0) {
+		trace(curl_err_buf);
+	}
+	
+	curl_easy_cleanup(curl);
+	free(method_url);
+	trace("Http thread exit\n");
 }
 
 void 
 on_search(GtkWidget *widget, GtkWidget *entry) {
 	if (http_tid) {
+		trace("Killing http thread");
 		deadbeef->thread_detach(http_tid);
 		http_tid = 0;
 	}
